58cff6bd39a7972e7072fbb02357e5c45a397d46
Joshuajam88 <87989775+Joshuajam88@users.noreply.github.com>
2022-09-27 12:43:36 +0100

    fin

---
+++ java.js
@@ -0,0 +1,12 @@
+AOS.init() 
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
